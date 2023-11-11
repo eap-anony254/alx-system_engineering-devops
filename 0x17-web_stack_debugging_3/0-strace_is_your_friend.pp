1acfa350502ff5fd358dcfc27feaf9f75eb399d9
billwk254 <billkihungu51@gmail.com>
2023-11-11 13:32:36 +0300

    Puppy code to fix apache error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,7 @@
 # Renames misspelled .php extension
 
-exec{'fix-wordpress':
-command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
-path => '/usr/local/bin/:/bin'
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
+
