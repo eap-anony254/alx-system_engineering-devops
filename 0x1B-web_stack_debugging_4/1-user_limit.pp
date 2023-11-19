09cecd0d181afcbaa197fcfccac0947f2a370cc2
billwk254 <billkihungu51@gmail.com>
2023-11-19 09:16:54 +0300

    Fix to allow holberton user to login

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,12 @@
 # Puppet manifest to adjust file limits for the holberton user
 
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'ulimit -n 4096',  # Adjust the file limit as needed
-  path    => '/usr/local/bin/:/bin/',
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
-# Additional configurations can be added if needed
 
-# You might want to restart the session or apply additional changes here
--> exec { 'restart-session':
-  command => 'su - holberton',
-  path    => '/usr/local/bin/:/bin/',
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
+
